11363de3f0d228cc23c84ae87606f87e8fa7ec77
voverg <voverg81@mail.ru>
2022-04-15 13:19:57 +0300

    zhk: add counter functionality

---
+++ zhk/js/main.js
@@ -0,0 +1,30 @@
+// DOM-elements
+const houseEl = document.querySelector('.home-img');
+// Constants and variables
+const counter = new Counter(floors);
+
+// Create floor hovers elems
+function createFloors(floorList) {
+  const floorEls = floorList.map(floor => {
+    const className = floor.active ? 'current-floor' : '';
+
+    return `
+      <path data-floor="${floor.floor}" class="${className}" d="${floor.coord}" fill="#3595F6" fill-opacity="0.75"/>
+    `;
+  });
+
+  houseEl.insertAdjacentHTML('beforeend', floorEls.join(''));
+}
+
+createFloors(floors);
+
+// Work with house events
+function getUpdateFloors({target}) {
+  if (target.dataset.floor) {
+    counter.updateFloors(target);
+  }
+}
+
+houseEl.addEventListener('mouseover', getUpdateFloors);
+houseEl.addEventListener('click', getUpdateFloors);
+
