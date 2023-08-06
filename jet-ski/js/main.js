a52cb2afdbe99e9daba5701ea0d0af8529269a4b
voverg <voverg81@mail.ru>
2023-08-06 14:01:50 +0300

    Jet ski: Create a slider

--- jet-ski/js/main.js
+++ jet-ski/js/main.js
@@ -1,3 +1,8 @@
 $(function(){
+  $('.slider').slick({
+    dots: true,
+    prevArrow: `<button class="slider__btn slider__btn-prev"><img src="icons/arrow-left.svg"></button>`,
+    nextArrow: `<button class="slider__btn slider__btn-next"><img src="icons/arrow-right.svg"></button>`,
+  });
 
 });
