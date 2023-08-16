a5651b4627e83181271a105451d961280231c3e8
voverg <voverg81@mail.ru>
2023-08-16 15:14:22 +0300

    Jet ski: create a bunner section

--- jet-ski/js/main.js
+++ jet-ski/js/main.js
@@ -1,31 +1,3 @@
-$(function(){
-  // Create a promo slider
-  $('.promo__slider').slick({
-    dots: true,
-    prevArrow: `<button class="promo__slider-btn promo__slider-btn-prev">
-                  <img src="icons/arrow-left.svg">
-                </button>`,
-    nextArrow: `<button class="promo__slider-btn promo__slider-btn-next">
-                  <img src="icons/arrow-right.svg">
-                </button>`,
-  });
-
-  // Create a products slider
-  $('.products__slider').slick({
-    dots: false,
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    prevArrow: `<button class="products__slider-btn products__slider-btn-prev">
-                  <img src="icons/arrow-left-black.svg">
-                </button>`,
-    nextArrow: `<button class="products__slider-btn products__slider-btn-next">
-                  <img src="icons/arrow-right-black.svg">
-                </button>`,
-  });
-
-});
-
 // Handle search section tabs
 const searchTabs = new Tabs('.search');
 
@@ -43,3 +15,33 @@ function clickHandler(event) {
     event.preventDefault();
   }
 }
+
+// Sliders
+$(function(){
+  // Create a promo slider
+  $('.promo__slider').slick({
+    dots: true,
+    prevArrow: `<button class="promo__slider-btn promo__slider-btn-prev">
+                  <img src="icons/arrow-left.svg">
+                </button>`,
+    nextArrow: `<button class="promo__slider-btn promo__slider-btn-next">
+                  <img src="icons/arrow-right.svg">
+                </button>`,
+  });
+
+  // Create a products slider
+  $('.products__slider').slick({
+    dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: `<button class="products__slider-btn products__slider-btn-prev">
+                  <img src="icons/arrow-left-black.svg">
+                </button>`,
+    nextArrow: `<button class="products__slider-btn products__slider-btn-next">
+                  <img src="icons/arrow-right-black.svg">
+                </button>`,
+  });
+
+});
+
+
